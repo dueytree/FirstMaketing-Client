581bec0844339da20d2a6a9160a56496f4670c1f
dueytree <dueytree.00@gmail.com>
2021-06-04 14:26:18 +0900

    FMS-2, accounts 앱 생성 후 회원관리 기능 페이지 추가

---
+++ src/pages/accounts/Login.js
@@ -0,0 +1,103 @@
+import React, { useState } from "react";
+import { Card, Form, Input, Button, notification } from "antd";
+import { SmileOutlined, FrownOutlined } from "@ant-design/icons";
+import Axios from "axios";
+import { useHistory, useLocation } from "react-router-dom";
+import { useAppContext } from "store";
+import { setToken } from "store";
+import { parseErrorMessages } from "utils/forms";
+
+export default function Login() {
+  const { dispatch } = useAppContext();
+  const location = useLocation();
+  const history = useHistory();
+  const [fieldErrors, setFieldErrors] = useState({});
+
+  const { from: loginRedirectUrl } = location.state || {
+    from: { pathname: "/" },
+  };
+
+  const onFinish = (values) => {
+    async function fn() {
+      const { username, password } = values;
+
+      setFieldErrors({});
+
+      const data = { username, password };
+      try {
+        const response = await Axios.post(
+          "http://127.0.0.1:8000/accounts/token/",
+          data
+        );
+        const {
+          data: { token: jwtToken },
+        } = response;
+
+        dispatch(setToken(jwtToken));
+
+        notification.open({
+          message: "로그인 성공",
+          icon: <SmileOutlined style={{ color: "#108ee9" }} />,
+        });
+
+        history.push(loginRedirectUrl);
+      } catch (error) {
+        if (error.response) {
+          notification.open({
+            message: "로그인 실패",
+            description: "아이디/암호를 확인해주세요.",
+            icon: <FrownOutlined style={{ color: "#ff3333" }} />,
+          });
+
+          const { data: fieldsErrorMessages } = error.response;
+          setFieldErrors(parseErrorMessages(fieldsErrorMessages));
+        }
+      }
+    }
+    fn();
+  };
+
+  return (
+    <Card title="Login">
+      <Form {...layout} onFinish={onFinish}>
+        <Form.Item
+          label="Username"
+          name="username"
+          rules={[
+            { required: true, message: "Please input your username!" },
+            { min: 5, message: "5글자 입력해주세요." },
+          ]}
+          hasFeedback
+          {...fieldErrors.username}
+          {...fieldErrors.non_field_errors}
+        >
+          <Input />
+        </Form.Item>
+
+        <Form.Item
+          label="Password"
+          name="password"
+          rules={[{ required: true, message: "Please input your password!" }]}
+          {...fieldErrors.password}
+        >
+          <Input.Password />
+        </Form.Item>
+
+        <Form.Item {...tailLayout}>
+          <Button type="primary" htmlType="submit">
+            Submit
+          </Button>
+        </Form.Item>
+      </Form>
+    </Card>
+  );
+}
+
+const layout = {
+  labelCol: { span: 8 },
+  wrapperCol: { span: 16 },
+};
+
+const tailLayout = {
+  wrapperCol: { offset: 8, span: 16 },
+};
---
+++ src/pages/accounts/Profile.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function ProFile() {
+  return <div>accounts/profile</div>;
+}
+
+export default ProFile;
